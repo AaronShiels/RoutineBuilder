752a5de3cfc367cc2ef930a45f3f7e7d8d8af146
Aaron Shiels <aaron.shiels@outlook.com>
2015-01-17 14:15:33 +1000

    Angular working

---
+++ src/RoutineBuilder.Web/Scripts/App/rountine.js
@@ -0,0 +1,17 @@
+﻿var routineModule = angular.module('routineModule', []);
+
+routineModule.directive('rbRoutine', [function ()
+{
+    return {
+        restrict: 'E',
+        templateUrl: '/Templates/rbRoutine.html',
+        scope: {
+            name: '=',
+        },
+        controllerAs: 'ctrl',
+        controller: function ()
+        {
+        },
+        bindToController: true
+    }
+}]);
---
+++ src/RoutineBuilder.Web/Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="app/rountine.js" />
+/// <reference path="npm.js" />
+/// <reference path="jquery-2.1.3.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="angular.js" />
+/// <reference path="angular-mocks.js" />
