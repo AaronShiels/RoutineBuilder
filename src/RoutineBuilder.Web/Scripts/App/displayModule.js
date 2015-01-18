49d0d4768427fb96f74f4fda26265636a79ba5a4
Aaron Shiels <aaron.shiels@outlook.com>
2015-01-18 18:51:58 +1000

    Component architecture wired up and working, still mega debug

---
+++ src/RoutineBuilder.Web/Scripts/App/displayModule.js
@@ -0,0 +1,19 @@
+﻿angular.module('displayModule', [])
+    .directive('rbDisplay', function ()
+    {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: '/Templates/rbDisplay.html',
+            scope: {
+                trainingMaxes: '=',
+                routineDefinition: '='
+            },
+            controllerAs: 'ctrl',
+            controller: 'displayCtrl',
+            bindToController: true
+        }
+    })
+    .controller('displayCtrl', [function ()
+    {
+    }]);
---
+++ src/RoutineBuilder.Web/Scripts/App/mainModule.js
@@ -0,0 +1,23 @@
+﻿angular.module('mainModule', ['displayModule', 'providerModule'])
+    .directive('rbMain', function ()
+    {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: '/Templates/rbMain.html',
+            scope: {},
+            controllerAs: 'ctrl',
+            controller: 'mainCtrl',
+            bindToController: true
+        }
+    })
+    .controller('mainCtrl', ['routineProviderFactory', function (routineProviderFactory)
+    {
+        this.trainingMaxes = [{
+            'Bench Press': 110,
+            'Squat': 0,
+            'Deadlift': 162.5,
+            'Overhead Press' : 55
+        }];
+        this.routineDefinition = routineProviderFactory.getRoutineDefinitionByName('Beyond 531');
+    }]);
